70ba3224e0227eadc5642970ea2367bd10530cfa
m70b1jr <corbin.titus@my.lr.edu>
2020-10-19 15:58:17 -0400

    Moreu updates

---
+++ Assets/HandPresence.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public List<GameObject> controllerPrefabs;
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+            //Log the movement
+           // Debug.Log(item.name + item.characteristics);
+        }
+
+        //Changes Model Based On VR HeadSet Used
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if(prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                //If we cant find the model, let the console know so we can change the naming.
+                   UnityEngine.Debug.Log("Could Not Find Right Model");
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The following code logs keypresses for debugging
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+            UnityEngine.Debug.Log("Pressing Primary Button");
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+            UnityEngine.Debug.Log("Trigger Pressed " + triggerValue);
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
+            UnityEngine.Debug.Log("Primary TouchPad" + primaryButtonValue);
+ }
+}
